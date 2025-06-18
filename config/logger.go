992a01c3edfe3785ac2b06be7b6c17ee698f270a
RLRama <rl.ramiro11@gmail.com>
2025-06-18 13:02:03 -0300

    feat(WIP): add logging level

--- config/logger.go
+++ config/logger.go
@@ -1,16 +1,29 @@
 package config
 
 import (
+	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 )
 
 func SetupLogger() {
-	output := log.Output(zerolog.ConsoleWriter{
+	output := zerolog.ConsoleWriter{
 		Out:        os.Stdout,
-		TimeFormat: time.RFC3339})
+		TimeFormat: time.RFC3339,
+	}
 
+	output.FormatLevel = func(i interface{}) string {
+		return strings.ToUpper(fmt.Sprintf("| %-6s |", i))
+	}
+
+	output.FormatFieldName = func(i interface{}) string {
+		return fmt.Sprintf("%s: ", fmt.Sprintf("%s", i))
+	}
+
+	output.FormatFieldValue = func(i interface{}) string {
+
+	}
 }
